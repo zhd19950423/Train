95e551b5575cedc75dede7867276732ed2d982a2
zhd19950423 <1037227472@qq.com>
2018-03-12 12:29:08 +0800

    二进制中1的个数

---
+++ train_11.java
@@ -0,0 +1,19 @@
+package offer_train;
+/*
+ * 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+ */
+public class train_11 {
+	public int NumberOf1(int n) {
+		int count = 0;
+        while(n!= 0){
+            count++;
+            n = n & (n - 1);
+         }
+        return count; 
+    }
+	public static void main(String[] args) {
+		train_11 rTrain_11 = new train_11();
+		System.out.println(rTrain_11.NumberOf1(-9));
+		
+	}
+}
